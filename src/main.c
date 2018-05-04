9f1710c7ceeb70cea58e14abf63a5561585664ee
Jorge Blázquez Saborido <jorge.jbs@protonmail.com>
2018-05-04 15:03:28 +0200

    Add a more elaborate test

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 
 #define CYCLES 3
 #define WARRIORS 2
-#define CORESIZE 7*WARRIORS // 14
+#define CORESIZE 35
 
 int main() {
     struct mars mars;
@@ -16,20 +16,46 @@ int main() {
         mars.core->core[i] = (struct instruction) {
             .opcode = DAT,
             .modifier = F,
-            .a = { .mode = IMMEDIATE, .number = 0 },
-            .b = { .mode = IMMEDIATE, .number = 0 }
+            .a = { .mode = IMMEDIATE, .number = 1 },
+            .b = { .mode = IMMEDIATE, .number = 1 }
         };
     }
 
-    for(int i = 0; i < mars.wlist->nwrs; i++) {
-        mars.core->core[CORESIZE/WARRIORS*i] = (struct instruction) {
-            .opcode = MOV,
-            .modifier = I,
-            .a = { .mode = DIRECT, .number = 0 },
-            .b = { .mode = DIRECT, .number = 1 }
-        };
-        queue_push_back(&mars.wlist->l[i], CORESIZE/WARRIORS*i);
-    }
+    // WARRIOR 1
+    mars.core->core[0] = (struct instruction) {
+        .opcode = ADD,
+        .modifier = AB,
+        .a = { .mode = IMMEDIATE, .number = 4 },
+        .b = { .mode = DIRECT, .number = 3 }
+    };
+    mars.core->core[1] = (struct instruction) {
+        .opcode = MOV,
+        .modifier = I,
+        .a = { .mode = DIRECT, .number = 2 },
+        .b = { .mode = INDIRECT, .number = 2 }
+    };
+    mars.core->core[2] = (struct instruction) {
+        .opcode = JMP,
+        .modifier = B,
+        .a = { .mode = DIRECT, .number = CORESIZE - 2 },
+        .b = { .mode = DIRECT, .number = 1 }
+    };
+    mars.core->core[3] = (struct instruction) {
+        .opcode = DAT,
+        .modifier = F,
+        .a = { .mode = IMMEDIATE, .number = 0 },
+        .b = { .mode = IMMEDIATE, .number = 0 }
+    };
+    queue_push_back(&mars.wlist->l[0], 0);
+
+    // WARRIOR 2
+    mars.core->core[7] = (struct instruction) {
+        .opcode = MOV,
+        .modifier = I,
+        .a = { .mode = DIRECT, .number = 0 },
+        .b = { .mode = DIRECT, .number = 1 }
+    };
+    queue_push_back(&mars.wlist->l[1], 7);
 
     core_print(mars.core);
     printf("\n");
